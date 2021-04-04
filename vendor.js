bb6c9e5f6d10bbf9f3218d05cbc23c646bb55f2d
Cody Carpenter <cody_carpenter2222@yahoo.com>
2021-04-03 21:30:15 -0700

    done

--- vendor.js
+++ vendor.js
@@ -1,12 +1,18 @@
 'use strict';
 
-const store = process.env.STORE;
 const io = require('socket.io-client')
-const host = 'http://localhost:3001'
-const socket = io.connect(host)
-const faker = require('faker');
+const host = 'http://localhost:3000'
 
-setInterval(() => {
+const socket = io.connect(host)
+const caps = io.connect(`${host}/caps`);
+
+const faker = require('faker');
+require('dotenv').config();
+
+
+const store = process.env.STORE;
+
+setInterval( () => {
   let entry = {
     store: store,
     orderID: faker.datatype.uuid(),
@@ -14,11 +20,14 @@ setInterval(() => {
     address: `${faker.address.city()}, ${faker.address.stateAbbr()}`
 
   };
-    socket.emit('pickup', entry)
+    caps.emit('pickup', entry)
 }, 5000);
 
-socket.on('delivered', (payload) => {
- console.log('VENDOR: thank you for delivering', payload.orderID);
-})
+caps.on('delivered', confirmDelivery);
+
+function confirmDelivery(payload) {
+  console.log(`VENDOR: thank you for delivery ${payload.orderID}`)
+}
+
 
 console.log('VENDOR CONNECTED');
